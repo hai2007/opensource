aff65b934805a06736087158835e86a026023fb7
你好2007 <2501482523@qq.com>
2022-04-16 13:29:32 +0800

    新增“一些插件”小节

--- build/main@v0.1.2-bundle4.js
+++ build/main@v0.1.2-bundle4.js
@@ -41,7 +41,7 @@ __etcpack__scope_bundle__.default=_class;
 window.__etcpack__bundleSrc__['40']=function(){
     var __etcpack__scope_bundle__={};
     var __etcpack__scope_args__;
-    __etcpack__scope_bundle__.default= ""
+    __etcpack__scope_bundle__.default= "\n .view>h2{\n\ntext-align: center;\n\nbackground-position: center center;\n\nmargin: 10px 0;\n\nbackground-size: auto 100%;\n\nline-height: 70px;\n\nbackground-repeat: no-repeat;\n\nfont-size: 14px;\n\ncolor: white;\n\n}\n\n .view>h2.library{\n\nbackground-image: url('./image/title-bg-2.png');\n\n}\n\n .view>h2.plug{\n\nbackground-image: url('./image/title-bg-1.png');\n\n}\n\n .view .plug-list{\n\ntext-align: center;\n\n}\n\n .view .plug-list>a{\n\ndisplay: inline-block;\n\nwidth: 300px;\n\npadding-top: 150px;\n\nbackground-repeat: no-repeat;\n\nbackground-position: center top;\n\nmargin: 10px;\n\nline-height: 2em;\n\nfont-size: 12px;\n\ncursor: pointer;\n\ncolor: rgb(0, 0, 0);\n\nborder-width: 5px 0px;\n\nborder-style: solid;\n\n}\n\n .view .plug-list>a.editor{\n\nbackground-image: url('./image/editor.jpg');\n\nborder-color: rgb(13, 181, 52);\n\n}\n\n .view .plug-list>a.excel{\n\nbackground-image: url('./image/excel.jpg');\n\nborder-color: rgb(23, 187, 228);\n\n}\n\n .view .plug-list>a:hover{\n\ntext-decoration: underline;\n\nfont-weight: 800;\n\n}\n"
   
     return __etcpack__scope_bundle__;
 }
@@ -52,7 +52,7 @@ window.__etcpack__bundleSrc__['40']=function(){
 window.__etcpack__bundleSrc__['41']=function(){
     var __etcpack__scope_bundle__={};
     var __etcpack__scope_args__;
-    __etcpack__scope_bundle__.default= "<span style=\"font-family:serif;display:block;text-align:center;padding:50px;font-size: 20px;color:red;\">\n    温馨提示：本页面设计开发中~\n\n    <!-- 在本页说明一些基础技术和包等 -->\n\n</span>\n"
+    __etcpack__scope_bundle__.default= "<div class=\"view\">\n    <!-- <h2 class=\"library\">基础依赖</h2> -->\n    <div class=\"library-list\">\n\n    </div>\n    <h2 class=\"plug\">一些插件</h2>\n    <div class=\"plug-list\">\n        <a class=\"editor\" target=\"_blank\" href=\"https://hai2007.gitee.io/open-web-editor/test/index.html\">\n            Open Web Editor\n        </a>\n        <a class=\"excel\" target=\"_blank\" href=\"https://hai2007.gitee.io/open-web-excel/test/index.html\">\n            Open Web Excel\n        </a>\n    </div>\n</div>\n"
   
     return __etcpack__scope_bundle__;
 }
--- src/page/main/basic/index.ts
+++ src/page/main/basic/index.ts
@@ -9,4 +9,5 @@ import template from './index.html'
 })
 export default class {
 
+
 }
